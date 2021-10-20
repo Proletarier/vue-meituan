f7403cb19ab22bdb9e70006f6a018b19f584c9c0
wenheng <wenheng4034186@163.com>
2021-10-20 14:34:33 +0800

    登录页面

--- src/page/login/index.vue
+++ src/page/login/index.vue
@@ -9,8 +9,8 @@
    <div class="login-form">
      <div class="login-wrapper">
        <div class="login-phone-num-wrapper">
-         <input class="login-phone-num-input" id="phoneNumInput" type="tel" autocomplete="off" placeholder="请输入手机号" maxlength="11"/>
-         <div class="login-send-verify-code-text">
+         <input class="login-phone-num-input" :value="phone" id="phoneNumInput" type="tel" autocomplete="off" placeholder="请输入手机号" maxlength="11"/>
+         <div class="login-send-verify-code-text" :class="{'login-active': phone.length === 11}">
            <span id="sendCodeBtnText">发送验证码</span>
            <div class="iloginLoading">
              <div class="sms-circle"></div>
@@ -18,7 +18,26 @@
          </div>
        </div>
        <div class="login-verifty-code-wrapper">
-         <input class="login-code-input" id="codeInput" type="number" maxlength="6" autocomplete="off" placeholder="请输入短信验证码"/>
+         <input class="login-code-input" :value="captcha" id="codeInput" type="number" maxlength="6" autocomplete="off" placeholder="请输入短信验证码"/>
+       </div>
+       <div class="input-opbtn-wrapper">
+          <button class="input-login-btn-wrapper" id="iloginBtn">
+            登录
+          </button>
+       </div>
+      <div class="login-user-confirm">
+         <div class="login-user-confirm-wrapper">
+           <label id="loginUserConfirm">
+             <input type="checkbox" class="checkbox" id="inputChecked" value="" autocomplete="off">
+             <i></i>
+           </label>
+           <div class="login-user-confirm-text login-user-account-common">
+            我已阅读并同意
+            <a href="https://rules-center.meituan.com/m/detail/4" style="color: #3488FF">《美团用户协议》、</a>
+            <a href="https://rules-center.meituan.com/m/detail/2" style="color: #3488FF">《隐私政策》</a>
+            ，并授权美团使用该美团账号信息（如昵称、头像、收货地址）进行统一管理
+           </div>
+         </div>
        </div>
      </div>
    </div>
@@ -26,6 +45,14 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      phone: '',
+      captcha: ''
+    };
+  }
+};
 </script>
 
 <style lang="stylus">
@@ -78,9 +105,13 @@
         display: flex
         color: #000
         align-items: center
-        padding: .2em .5em
+        padding: .4em .5em
         border-radius: .2em
         cursor pointer
+        font-size: 1.1em
+      .login-active
+        color #ffd300
+        border 1px solid #ffd300
     .login-verifty-code-wrapper
       display: flex
       justify-content: space-around
@@ -99,4 +130,41 @@
         border 0
         font-size 1em
         min-width 0
+    .input-opbtn-wrapper
+      margin-top: 1em
+      .input-login-btn-wrapper
+       text-align: center
+       color: #999
+       background-color: #e5e5e5
+       height: 2.6em
+       line-height: 2.6em
+       font-size: 1em
+       display: block
+       width 100%
+       border-radius: .3em
+       border: none
+       cursor pointer
+       user-select:none
+    .login-user-confirm
+      padding-top: 2.3em
+      .login-user-confirm-wrapper
+        display: flex
+        label
+          display: inline-block
+          min-width: 10%
+          .checkbox
+            display: none
+          .checkbox[type=checkbox]+i
+            display: inline-block
+            border: 1px solid #ccc
+            width 17px
+            height: 17px
+            border-radius: 50%
+            background-color: #fff
+            position relative
+            margin 2px 10px
+        .login-user-confirm-text
+          color: rgba(0,0,0,.5)
+          font-weight: 400
+          font-size: .85em
 </style>
